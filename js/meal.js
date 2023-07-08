8fe753b24af96a9ba76f3a4c9dd4a2ab6342dc91
Murad-Takla <irfanchowdhurycr7@gmail.com>
2023-07-08 16:48:53 +0600

    All done

--- js/meal.js
+++ js/meal.js
@@ -8,16 +8,19 @@ const meal = (search) => {
 
 
 const displayMeals = (meals) => {
+
    
+    // console.log(meals)
+
     const mealsContainer = document.getElementById('meals-container')
     mealsContainer.innerHTML=``
     meals.forEach(element => {
+        console.log(element.idMeal)
 
         const div = document.createElement('div')
-        
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick="detailsMeal(${element.idMeal})"class="card h-100">
         <img src="${element.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${element.strMeal}</h5>
@@ -34,7 +37,7 @@ function searchFood() {
     const searchField= document.getElementById('search-field')
     const searchItem  = searchField.value
     searchField.value=''
-    console.log('asma'+searchItem+'irfan')
+    
 
     if (searchItem==''){
         alert ('Please Enter Your Food')
@@ -45,3 +48,35 @@ function searchFood() {
     }
      
 }
+
+
+const detailsMeal = idMeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+     
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]))
+    
+}
+
+const displayMealDetail = (meal) =>{
+    console.log(meal)
+
+    const divDetail = document.getElementById('details-meal')
+
+    divDetail.innerHTML = `
+    <div class="card container" style="width: 18rem;">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h4 class="card-text">${meal.strMeal}</h4>
+                  <blockquote>${meal.strInstructions.slice(0,200)}</blockquote>
+                </div>
+              </div>
+              `
+}
+
+const showOneDetail = () => {
+    const divDetail = document.getElementById('details-meal')
+    divDetail.innerHTML = ``
+}
+
